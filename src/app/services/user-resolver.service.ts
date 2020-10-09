abe2f2239ed0b768519b0e3f64064dc215ee2bcd
robb <roberto.manganelly@gmail.com>
2020-10-09 00:29:15 -0400

    photo request implemented

--- src/app/services/user-resolver.service.ts
+++ src/app/services/user-resolver.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, Resolve, RouterStateSnapshot } from '@angular/router';
 import { Observable } from 'rxjs';
 import { LoggedUser } from '../models/user.interface';
+import { API_VERSION, SERVER_URL } from './../../assets/paths';
 
 @Injectable({
   providedIn: 'root'
@@ -14,6 +15,17 @@ export class UserResolverService implements Resolve<LoggedUser> {
     activatedRouteSnapshot: ActivatedRouteSnapshot,
     routerStateSnapshot: RouterStateSnapshot
     ): Observable<LoggedUser> | Promise<LoggedUser> | LoggedUser {
-    return JSON.parse(localStorage.getItem('userData'));
+    const user =  JSON.parse(localStorage.getItem('userData'));
+    if (user.photo){
+      user.photo = `${SERVER_URL}/${user.photo}`;
+      console.log(user.photo);
+    }
+    user.contacts.forEach(contact => {
+      if (contact.photo){
+        contact.photo = `${SERVER_URL}/${contact.photo}`;
+      };
+    });
+
+    return user;
   }
 }
